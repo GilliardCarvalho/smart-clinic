992546d78b458cc6d15b6ad0ea4327be10b3f420
GilliardCarvalho <gilliard.carvalho6@gmail.com>
2025-07-21 19:27:57 -0300

    Add AppointmentService

---
+++ src/main/java/com/smartclinic/service/AppointmentService.java
@@ -0,0 +1,52 @@
+package com.smartclinic.service;
+
+import com.smartclinic.model.Appointment;
+import com.smartclinic.repository.AppointmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AppointmentService {
+
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+
+    // Retorna todos os agendamentos
+    public List<Appointment> getAllAppointments() {
+        return appointmentRepository.findAll();
+    }
+
+    // Retorna um agendamento por ID
+    public Optional<Appointment> getAppointmentById(Long id) {
+        return appointmentRepository.findById(id);
+    }
+
+    // Cria um novo agendamento
+    public Appointment createAppointment(Appointment appointment) {
+        return appointmentRepository.save(appointment);
+    }
+
+    // Atualiza um agendamento existente
+    public Optional<Appointment> updateAppointment(Long id, Appointment updatedAppointment) {
+        return appointmentRepository.findById(id)
+                .map(appointment -> {
+                    appointment.setDateTime(updatedAppointment.getDateTime());
+                    appointment.setReason(updatedAppointment.getReason());
+                    appointment.setDoctor(updatedAppointment.getDoctor());
+                    appointment.setPatient(updatedAppointment.getPatient());
+                    return appointmentRepository.save(appointment);
+                });
+    }
+
+    // Deleta um agendamento
+    public boolean deleteAppointment(Long id) {
+        if (appointmentRepository.existsById(id)) {
+            appointmentRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
